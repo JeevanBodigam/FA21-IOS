3cf87aa4578202d764b5c703779d90e5c3cb1c50
JeevanBodigam <bodigamjeevan296@gmail.com>
2021-08-19 14:25:59 -0500

    first day code

---
+++ firstDay.playground/Contents.swift
@@ -0,0 +1,10 @@
+var a = 10
+var b = 20
+var sum = 0
+sum = a + b
+print("The sum of given two numbers is \(sum)")
+
+var prod = 0.0
+
+prod = (Double)(a * b)
+print("The product of given 2 numbers is \(prod)")
